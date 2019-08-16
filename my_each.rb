966a482a5a092bb2bf289a529a3ca0c086386737
Hanna Lee <mani21na@gmail.com>
2019-08-16 02:11:46 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
+#=begin
+def my_each(array) # put argument(s) here
   # code here
+  i = 0
+  while i < array.length
+    yield array[i]
+    i += 1
+  end
+  array
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
